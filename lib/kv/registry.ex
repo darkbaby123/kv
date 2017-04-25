efc011778600b2eebd796bdab5bbe8cb74f7af25
David Chen <darkbaby123@gmail.com>
2017-04-25 19:42:28 +0800

    Refactor KV.Registry with GenServer

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,16 +1,51 @@
 defmodule KV.Registry do
+  use GenServer
+
+  ### Server
+
+  def init(_) do
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
+  end
+
+  def handle_call({:lookup, name}, _, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_cast({:create, name}, {names, refs} = state) do
+    if Map.has_key?(names, name) do
+      {:noreply, state}
+    else
+      {:ok, bucket} = KV.Bucket.start_link()
+      ref = Process.monitor(bucket)
+      new_names = Map.put(names, name, bucket)
+      new_refs = Map.put(refs, ref, name)
+      {:noreply, {new_names, new_refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _, _}, {names, refs}) do
+    {name, new_refs} = Map.pop(refs, ref)
+    new_names = Map.delete(names, name)
+    {:noreply, {new_names, new_refs}}
+  end
+
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
+  ### Client
+
   def start_link do
-    Agent.start_link(fn -> %{} end)
+    GenServer.start_link(__MODULE__, :ok)
   end
 
   def lookup(registry, name) do
-    Agent.get(registry, &Map.fetch(&1, name))
+    GenServer.call(registry, {:lookup, name})
   end
 
   def create(registry, name) do
-    Agent.cast(registry, fn names ->
-      {:ok, bucket} = KV.Bucket.start_link()
-      Map.put(names, name, bucket)
-    end)
+    GenServer.cast(registry, {:create, name})
   end
 end
