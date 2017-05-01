fa54cfc51c5e29312dc9b466d167a9123bc5c129
David Chen <darkbaby123@gmail.com>
2017-05-01 22:18:52 +0800

    Add gen_tcp ecto server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,35 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for KVServer.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    # 把 serve 放到 task supervisor 里去执行
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    # 修改 client 关联的 process ，当 client 挂掉后会由那个 process 处理异常
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello
-      :world
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
 
-  """
-  def hello do
-    :world
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, message} = :gen_tcp.recv(socket, 0)
+    message
+  end
+
+  defp write_line(message, socket) do
+    :gen_tcp.send(socket, message)
   end
 end
